4cacae5b831039a8e6a95bfb804b1b1de07da1bd
Matthew Roy <matthew@saplink.io>
2023-03-10 04:14:53 +0000

    init cargo and add lib file, add get var function

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use std::env;
+
+pub fn get_env_var(desired_env_var: &str) -> String {
+    match env::var(desired_env_var) {
+        Ok(v) => v,
+        Err(e) => panic!("${} is not set ({})", desired_env_var, e),
+    }
+}
